5ea1c067c46c79ea39bd0926f7661f2153bb1c9e
Rodolfo <rgouvert@gmail.com>
2021-11-04 07:04:21 -0300

    tochi

---
+++ options/mariaDB.js
@@ -0,0 +1,11 @@
+const options ={
+    client : 'mysql',
+    connection:{
+        host:'127.0.0.1',
+        user:'root',
+        password:'r0d0',
+        database:'node'
+    }
+}
+
+module.exports={options}
